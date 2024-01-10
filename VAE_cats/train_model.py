e57e8e537750a9ff46b9dac679519416f3baf95d
Kristoffer Nielsen <s194644@dtu.dk>
2024-01-10 13:01:11 +0100

    train_model

--- VAE_cats/train_model.py
+++ VAE_cats/train_model.py
@@ -1,8 +1,18 @@
 import torch 
+from pytorch_lightning import Trainer
+from models.model import Model
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
 
-from models.model import Encoder, Decoder, Model
-
-encoder = Encoder(input_dim=x_dim, hidden_dim=hidden_dim, latent_dim=latent_dim)
-decoder = Decoder(latent_dim=latent_dim, hidden_dim=hidden_dim, output_dim=x_dim)
-
-model = Model(encoder=encoder, decoder=decoder).to(DEVICE)
+dataset = DataLoader(torch.load("data/processed/cats.pt"), batch_size=64, shuffle=True)
+model = Model()
+checkpoint_callback = ModelCheckpoint(
+    dirpath="./models", monitor="val_loss", mode="min"
+)
+early_stopping_callback = EarlyStopping(
+    monitor="val_loss", patience=3, verbose=True, mode="min"
+)
+trainer = Trainer(callbacks=[early_stopping_callback, checkpoint_callback],logger=pl.loggers.WandbLogger(project="dtu_mlops"))
+trainer.fit(model,dataset)
+trainer.save_checkpoint("models/model.ckpt")
