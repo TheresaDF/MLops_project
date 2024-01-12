bba613930da71fa41f171ce5cc31735d0922118f
TheresaDF <s194329@student.dtu.dk>
2024-01-12 11:56:26 +0100

    visualize not makes an image

--- VAE_cats/predict_model.py
+++ VAE_cats/predict_model.py
@@ -6,8 +6,11 @@ import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
 
-model = Model.load_from_checkpoint("models/epoch=0-step=27.ckpt")
+model = Model.load_from_checkpoint("models/epoch=4-step=135.ckpt")
 with torch.no_grad():
     noise = torch.randn(64, 8, 2, 2)
     images = model.decode(noise)
 save_image(images.view(64, 3, 128, 128), "generated_sample.png")
+
+def predict():
+    pass 
--- VAE_cats/visualizations/visualize.py
+++ VAE_cats/visualizations/visualize.py
@@ -6,16 +6,14 @@ from vae_cats.models.model import Model
 # # # # Declare constants # # # #
 DATA_PATH = "data/processed/cats.pt"
 MODEL_PATH = "models/epoch=4-step=135.ckpt"
-NR_OF_CATS = 100 
+NR_OF_CATS = 2000
 IMAGE_SIZE = (128, 128, 3)
 
 # init data 
-images = torch.load(DATA_PATH)
-dataset = torch.load("data/processed/cats.pt").float()[:NR_OF_CATS]
+dataset = torch.load(DATA_PATH).float()[:NR_OF_CATS].permute(0, 3, 1, 2)
 
 # init model 
-vae = Model()
-vae.load_state_dict(torch.load(MODEL_PATH)["model_state_dict"])
+vae = Model.load_from_checkpoint(MODEL_PATH)
 vae.eval()
 
 # inference 
