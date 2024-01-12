8a043f138f03aac866ee25827921a1608c10ab2a
AnaTeodoraR <s194324@student.dtu.dk>
2024-01-12 12:22:48 +0100

    smaller set-up changes

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,8 +6,5 @@ from tests import _PATH_DATA
 @pytest.mark.skipif(not os.path.exists(os.path.join(_PATH_DATA,"processed/cats.pt")), reason="Data files not found")
 def test_data():
     dataset = torch.load(os.path.join(_PATH_DATA,"processed/cats.pt"))
-    # check dataset length 
     assert len(dataset) == 1706, "Dataset did not have the correct number of samples"
-    # check image shape 
-    for img in dataset:
-        assert img.shape == (128,128,3), "Image in the dataset does not have the correct shape"
+    assert dataset[0].shape == (128,128,3), "Image in the dataset does not have the correct shape"
