50bf3fb910cec35b2bac48fa5aa31a3e61a45dc7
AnaTeodoraR <s194324@student.dtu.dk>
2024-01-12 11:17:10 +0100

    hydra configuration fixes

--- VAE_cats/data/make_dataset.py
+++ VAE_cats/data/make_dataset.py
@@ -11,12 +11,12 @@ import hydra
 # PROCESSED_PATH = "data/processed/cats.pt"
 # OUTPUT_SIZE = (128, 128, 3)
 
-@hydra.main(config_path="../../conf", config_name="config.yaml")
+@hydra.main(config_path="../../conf", config_name="config.yaml", version_base=None)
 def process_data(cfg) -> None:
     """ Pre-processes the raw data of cat images. """
     data_params = cfg.data
     output_size = (data_params.xy_dim, data_params.xy_dim, data_params.channels)
-
+  
     # get all filenames 
     raw_files = glob.glob(os.path.join(data_params.data_path_raw, '*jpg'))
     N = len(raw_files)
