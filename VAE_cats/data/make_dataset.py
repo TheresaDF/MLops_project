c6b41a5f6ceae8495c7d259665287e28f5e06609
TheresaDF <s194329@student.dtu.dk>
2024-01-12 10:18:37 +0100

    update make_dataset to be a function

--- VAE_cats/data/make_dataset.py
+++ VAE_cats/data/make_dataset.py
@@ -7,20 +7,24 @@ import numpy as np
 from tqdm import tqdm 
 
 # # # # Declare constants # # # 
-RAW_PATH = "data/raw"
-PROCESSED_PATH = "data/processed/cats.pt"
-OUTPUT_SIZE = (128, 128, 3)
+def process_data() -> None:
+    RAW_PATH = "data/raw"
+    PROCESSED_PATH = "data/processed/cats.pt"
+    OUTPUT_SIZE = (128, 128, 3)
 
-# get all filenames 
-raw_files = glob.glob(os.path.join(RAW_PATH, '*jpg'))
-N = len(raw_files)
-all_images = np.zeros((N, OUTPUT_SIZE[0], OUTPUT_SIZE[1], OUTPUT_SIZE[2]))
+    # get all filenames 
+    raw_files = glob.glob(os.path.join(RAW_PATH, '*jpg'))
+    N = len(raw_files)
+    all_images = np.zeros((N, OUTPUT_SIZE[0], OUTPUT_SIZE[1], OUTPUT_SIZE[2]))
 
-# read and resize images 
-for i, filename in tqdm(enumerate(raw_files)):
-    all_images[i] = resize(imread(filename), output_shape=OUTPUT_SIZE)
+    # read and resize images 
+    for i, filename in tqdm(enumerate(raw_files)):
+        all_images[i] = resize(imread(filename), output_shape=OUTPUT_SIZE)
 
-# save in correct format  
-all_images = torch.from_numpy(all_images)
-torch.save(all_images, PROCESSED_PATH)
+    # save in correct format  
+    all_images = torch.from_numpy(all_images)
+    torch.save(all_images, PROCESSED_PATH)
+
+if __name__ == "__main__":
+    process_data()
 
