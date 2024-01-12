84b187b9cebda28e2e34ded06a001757fccf4ca0
AnaTeodoraR <s194324@student.dtu.dk>
2024-01-12 11:22:39 +0100

    more comments

--- VAE_cats/train_model.py
+++ VAE_cats/train_model.py
@@ -9,17 +9,22 @@ import hydra
 @hydra.main(config_path="../conf", config_name="config.yaml",version_base=None)
 def train(cfg) -> None:
     """ Training the VAE model using pytorch lightning. """
+    # set up 
     hparams = cfg.experiments
-
     dataset = DataLoader(torch.load("data/processed/cats.pt").float(), batch_size=hparams.batch_size, shuffle=True)
     model = Model()
+    
+    # save best model 
     checkpoint_callback = ModelCheckpoint(
         dirpath="./models", monitor="train_loss", mode="min"
     )
+    # add early stopping
     early_stopping_callback = EarlyStopping(
         monitor="train_loss", patience=3, verbose=True, mode="min"
     )
+    # monitor the learning rate 
     lr_monitor = LearningRateMonitor(logging_interval='step')
+    # train
     trainer = Trainer(callbacks=[early_stopping_callback, checkpoint_callback,lr_monitor],logger=pl.loggers.WandbLogger(project="dtu_mlops"),log_every_n_steps=1, max_epochs=hparams.max_epochs)
     trainer.fit(model,dataset)
     
