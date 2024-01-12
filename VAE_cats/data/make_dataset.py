399ee252235c99469676329a9e163d599c72016a
TheresaDF <s194329@student.dtu.dk>
2024-01-12 09:26:33 +0100

    changed make_dataset back again

--- VAE_cats/data/make_dataset.py
+++ VAE_cats/data/make_dataset.py
@@ -5,12 +5,11 @@ from skimage.io import imread
 import torch 
 import numpy as np 
 from tqdm import tqdm 
-from matplotlib import pyplot as plt 
 
 # # # # Declare constants # # # 
 RAW_PATH = "data/raw"
 PROCESSED_PATH = "data/processed/cats.pt"
-OUTPUT_SIZE = (3, 128, 128)
+OUTPUT_SIZE = (128, 128, 3)
 
 # get all filenames 
 raw_files = glob.glob(os.path.join(RAW_PATH, '*jpg'))
@@ -19,7 +18,7 @@ all_images = np.zeros((N, OUTPUT_SIZE[0], OUTPUT_SIZE[1], OUTPUT_SIZE[2]))
 
 # read and resize images 
 for i, filename in tqdm(enumerate(raw_files)):
-    all_images[i] = resize(np.transpose(imread(filename), (1, 2, 0)), output_shape=OUTPUT_SIZE)
+    all_images[i] = resize(imread(filename), output_shape=OUTPUT_SIZE)
 
 # save in correct format  
 all_images = torch.from_numpy(all_images)
