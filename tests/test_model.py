f5fb92a335f718da76344739ba573c28efe34491
AnaTeodoraR <s194324@student.dtu.dk>
2024-01-15 13:37:17 +0100

    making model testing a bit faster (maybe)

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,11 +2,12 @@ import torch
 from vae_cats.models.model import Model
 
 def test_model():
-    model = Model()
-    noise = torch.randn((64,3,128,128))
-    # test model output shapes 
-    assert len(model(noise)) == 3, "Model expects 3 outputs: generated image, mean, and variance"
-    assert model(noise)[0].shape == (64,3,128,128), "Model did not generate correct image shape"
-    assert model(noise)[1].shape == (64,32), "Model did not have the expected latent size for mean"
-    assert model(noise)[2].shape == (64,32), "Model did not have the expected latent size for log var"
-    assert model.decode(torch.randn((64, 1024, 1, 1))).shape == (64,3,128,128), "Model did not decode to correct size"
+    model = Model(h_dim=1024, z_dim=32)
+    output = model(torch.randn((64,3,128,128)))
+    
+    # test model output shapes
+    assert len(output) == 3, "Model expects three outputs: generated image, mean, and variance"
+    assert output[0].shape == (64,3,128,128), "Model did not generate correct image shape"
+    assert output[1].shape == (64,32), "Model did not have the expected latent size for mean"
+    assert output[2].shape == (64,32), "Model did not have the expected latent size for log var"
+    assert model.decode(torch.randn((64,1024,1,1))).shape == (64,3,128,128), "Model did not decode noise to correct size"
