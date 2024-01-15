239c1f4fac6c63860e3fdce83cc1626f224a638a
AnaTeodoraR <s194324@student.dtu.dk>
2024-01-15 13:31:19 +0100

    fixed error in testing

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,5 +3,10 @@ from vae_cats.models.model import Model
 
 def test_model():
     model = Model()
-    # test model output shape 
-    assert model(torch.randn((64,3,128,128))).shape == (64,3,128,128), "Model did not output the expected shape"
+    noise = torch.randn((64,3,128,128))
+    # test model output shapes 
+    assert len(model(noise)) == 3, "Model expects 3 outputs: generated image, mean, and variance"
+    assert model(noise)[0].shape == (64,3,128,128), "Model did not generate correct image shape"
+    assert model(noise)[1].shape == (64,32), "Model did not have the expected latent size for mean"
+    assert model(noise)[2].shape == (64,32), "Model did not have the expected latent size for log var"
+    assert model.decode(torch.randn((64, 1024, 1, 1))).shape == (64,3,128,128), "Model did not decode to correct size"
