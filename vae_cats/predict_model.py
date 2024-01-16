dfe78b4565f04ca77489783f701d724e45e7a9ce
TheresaDF <s194329@student.dtu.dk>
2024-01-16 13:02:34 +0100

    predict uses argparse now

--- vae_cats/predict_model.py
+++ vae_cats/predict_model.py
@@ -2,6 +2,7 @@ import torch
 from models.model import Model
 from torchvision.utils import save_image
 import os
+import argparse
 
 def predict(model_path: str) -> None:
     """ Generate cat images from white noise using a trained model. 
@@ -21,4 +22,8 @@ def predict(model_path: str) -> None:
     save_image(images.view(64, 3, 128, 128), "reports/figures/generated_sample.png")
     
 if __name__ == "__main__":
-    predict("models\epoch=42-step=1161.ckpt")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", default = "models/epoch=0-step=27.ckpt", type = str)
+    args = parser.parse_args()
+
+    predict(args.model)
