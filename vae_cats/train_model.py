8d586a4a8e3a317d8555ffd06c46925dfef944f2
TheresaDF <s194329@student.dtu.dk>
2024-01-16 12:39:54 +0100

    added Kornia to requirements

--- vae_cats/train_model.py
+++ vae_cats/train_model.py
@@ -13,21 +13,25 @@ def train(cfg) -> None:
     hparams = cfg.experiments
     data_params = cfg.data
     dataset = DataLoader(torch.load("data/processed/cats.pt").float(), batch_size=hparams.batch_size, shuffle=True, num_workers = 4)
-    
     model = Model(image_channels=data_params.channels, h_dim=hparams.h_dim, z_dim=hparams.z_dim, lr=hparams.lr)
     
     # save best model 
-    checkpoint_callback = ModelCheckpoint(
-        dirpath="./models", monitor="train_loss", mode="min"
-    )
+    checkpoint_callback = ModelCheckpoint(dirpath="./models", monitor="train_loss", mode="min")
+
     # add early stopping
-    early_stopping_callback = EarlyStopping(
-        monitor="train_loss", patience=10, verbose=True, mode="min"
-    )
+    early_stopping_callback = EarlyStopping(monitor="train_loss", patience=10, verbose=True, mode="min")
+
     # monitor the learning rate 
     lr_monitor = LearningRateMonitor(logging_interval='step')
+
     # train
-    trainer = Trainer(callbacks=[early_stopping_callback, checkpoint_callback,lr_monitor],logger=pl.loggers.WandbLogger(project="dtu_mlops"),log_every_n_steps=10, max_epochs=hparams.max_epochs)
+    trainer = Trainer(callbacks=[early_stopping_callback, checkpoint_callback,lr_monitor],
+                      logger=pl.loggers.WandbLogger(project="dtu_mlops"),
+                      log_every_n_steps=10, 
+                      max_epochs=hparams.max_epochs, 
+                      devices = 8,
+                      accelerator = 'auto')
+    
     trainer.fit(model,dataset)
     
 if __name__ == "__main__":
