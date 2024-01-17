92732d702042b11f83d4eba5652afcbcf42d963f
AnaTeodoraR <s194324@student.dtu.dk>
2024-01-17 13:46:40 +0100

    added more tests on processed data

--- tests/test_data.py
+++ tests/test_data.py
@@ -8,3 +8,4 @@ def test_data():
     dataset = torch.load(os.path.join(_PATH_DATA,"processed/cats.pt"))
     assert len(dataset) == 1706, "Dataset did not have the correct number of samples"
     assert dataset[0].shape == (128,128,3), "Image in the dataset does not have the correct shape"
+    assert torch.sum((dataset < 0) | (dataset > 1)).item() == 0, "Image pixels must be in [0;1]"
