438923e44115b5bc87139359d47839ef56fcbd4c
Kristoffer Nielsen <s194644@dtu.dk>
2024-01-18 09:28:07 +0100

    fast_api

--- fast_api_app.py
+++ fast_api_app.py
@@ -3,11 +3,14 @@ from fastapi import FastAPI
 from vae_cats.models.model import Model
 from fastapi.responses import Response
 import io
+from io import BytesIO
+from PIL import Image
+import numpy as np
 import imageio
 import torchvision
 
-# model = Model.load_from_checkpoint("models\epoch=42-step=1161.ckpt")
-model = Model.load_from_checkpoint("models/epoch=0-step=27.ckpt")
+model = Model.load_from_checkpoint("models\epoch=42-step=1161.ckpt")
+# model = Model.load_from_checkpoint("models/epoch=0-step=27.ckpt")
 
 app = FastAPI()
 @app.get("/")
@@ -16,10 +19,9 @@ def show_image():
         noise = torch.randn(64, 1024, 1, 1) 
         images = model.decode(noise)
     grid_img = torchvision.utils.make_grid(images, nrow=8).permute(1,2,0).numpy()
-    with io.BytesIO() as output:
-        imageio.imwrite(output, grid_img, format='PNG')
-        data = output.getvalue()
-    #bigger size 
-    return Response(data, media_type="image/png")
+    pil_img = Image.fromarray((grid_img * 255).astype(np.uint8))
 
-
+    img_byte_array = BytesIO()
+    pil_img.save(img_byte_array, format='PNG')
+    img_byte_array.seek(0)
+    return Response(img_byte_array.read(), media_type="image/png")
